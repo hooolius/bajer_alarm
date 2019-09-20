f859dd151a6a7d45575e6db4552ff42cc10476c7
Julius Nyborg Olesen <hoolius.o@gmail.com>
2019-09-20 09:13:50 +0200

    added display options to all commands, should now work with crontab

--- balarm.sh
+++ balarm.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 display=$1
-geometry=( $(xdpyinfo | awk '/dimensions:/ { print $2; exit }' | tr "x" " ") )
+geometry=( $(xdpyinfo -display :0 | awk '/dimensions:/ { print $2; exit }' | tr "x" " ") )
 imagegeo=( 500 1000)
 offsetx=$(( (${geometry[0]} - ${imagegeo[0]})/2 ))
 offsety=$(( (${geometry[1]} - ${imagegeo[1]})/2 ))
@@ -16,7 +16,7 @@ trap "kill $sound 2> /dev/null" EXIT
 while kill -0 $sound 2> /dev/null; do
     echo $dir
     xgamma -display :0 -rgamma 4.0
-    xteddy -f $dir/BAJER.png -geometry 1x1+$offsetx+$offsety &
+    xteddy -f $dir/BAJER.png -geometry 1x1+$offsetx+$offsety -display :0 &
     sleep .5
     kill $!
     xgamma -display :0 -rgamma 1.0
