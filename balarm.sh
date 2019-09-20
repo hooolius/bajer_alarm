56fb4648db18a90b0d0c1f433c6e4f17676130ab
Julius Nyborg Olesen <hoolius.o@gmail.com>
2019-09-20 10:46:18 +0200

    fixed an issue where xteddy would not be found on Debian based systems

--- balarm.sh
+++ balarm.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 display=$1
+xteddys=( $(whereis xteddy) )
+xteddy=${xteddys[1]}
 geometry=( $(xdpyinfo -display :0 | awk '/dimensions:/ { print $2; exit }' | tr "x" " ") )
 imagegeo=( 500 1000)
 offsetx=$(( (${geometry[0]} - ${imagegeo[0]})/2 ))
@@ -14,9 +16,8 @@ sound=$!
 trap "kill $sound 2> /dev/null" EXIT
 
 while kill -0 $sound 2> /dev/null; do
-    echo $dir
     xgamma -display :0 -rgamma 4.0
-    xteddy -f $dir/BAJER.png -geometry 1x1+$offsetx+$offsety -display :0 &
+    $xteddy -f $dir/BAJER.png -geometry 1x1+$offsetx+$offsety -display :0 &
     sleep .5
     kill $!
     xgamma -display :0 -rgamma 1.0
